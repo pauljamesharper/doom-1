f0ac3cee8f73a7e7c91c3158ae54032d2eb57808
Linus <linus@sehn.tech>
2019-11-05 18:07:12 +0100

    added deft and removed after around flyspell blob

--- +flyspell.el
+++ +flyspell.el
@@ -1,9 +1,8 @@
 ;;; ~/.doom.d/+flyspell.el -*- lexical-binding: t; -*-
 
-(after! flyspell
-  (defun my/flyspell-save-word ()
-    (interactive)
-    (let ((current-location (point))
-          (word (flyspell-get-word)))
-      (when (consp word)
-        (flyspell-do-correct 'save nil (car word) current-location (cadr word) (caddr word) current-location)))))
+(defun my/flyspell-save-word ()
+  (interactive)
+  (let ((current-location (point))
+        (word (flyspell-get-word)))
+    (when (consp word)
+      (flyspell-do-correct 'save nil (car word) current-location (cadr word) (caddr word) current-location))))
