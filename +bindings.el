db7bf676ceb318615835635c9ca2854a422a7565
Linus <linus@sehn.tech>
2020-02-15 17:43:25 +0100

    polished org-clock-budget config

--- +bindings.el
+++ +bindings.el
@@ -6,18 +6,23 @@
       (:prefix-map ("r" . "org-ref")
         :desc "citation"               "c"     #'org-ref-insert-cite-with-completion
         :desc "pdf"                    "p"     #'org-ref-open-pdf-at-point
-        :desc "notes"                  "n"     #'org-ref-open-notes-at-point)
+        :desc "notes"                  "n"     #'org-ref-open-notes-at-point
+        )
 
       (:prefix-map ("d" . "dict")
         :desc "Add to dictionary"      "a"     #'my/flyspell-save-word
-        :desc "Change dictionary"      "d"     #'ispell-change-dictionary)
+        :desc "Change dictionary"      "d"     #'ispell-change-dictionary
+        )
 
       (:prefix-map ("a" . "anki")
         :desc "push notes to Anki"     "p"     #'anki-editor-push-notes
-        :desc "cloze region"           "c"     #'anki-editor-cloze-dwim)
+        :desc "cloze region"           "c"     #'anki-editor-cloze-dwim
+        )
 
       (:prefix-map ("i" . "insert")
-        :desc "Insert Anki notes"      "a"     #'anki-editor-insert-note))
+        :desc "Insert Anki notes"      "a"     #'anki-editor-insert-note
+        )
+      )
 
 ;; org-mode specific
 (map! :map org-mode-map
@@ -26,10 +31,16 @@
 
       (:localleader
         :desc "Hide property drawers"  "p"     #'my/org-cycle-hide-properties-everywhere
-        :desc "org-clock-budget"       "B"     #'org-clock-budget-report))
+        :desc "Show yearly budget"     "y"     #'show-yearly-clock-budget
+        :desc "Show yearly budget"     "m"     #'show-monthly-clock-budget
+        :desc "Show yearly budget"     "w"     #'show-weekly-clock-budget
+        )
+      )
 
 ;; pdf-tools specific
 (map! :map pdf-view-mode-map
       (:prefix-map ("M-a" . "annotate")
         :desc "Add highlight"          "h"    #'pdf-annot-add-highlight-markup-annotation
-        :desc "Add text annotation"    "t"    #'pdf-annot-add-text-annotation))
+        :desc "Add text annotation"    "t"    #'pdf-annot-add-text-annotation
+        )
+      )
--- +org-clock-budget.el
+++ +org-clock-budget.el
@@ -2,9 +2,34 @@
 
 (use-package! org-clock-budget
   :config
-  (setq org-clock-budget-intervals '(("BUDGET_WEEK" org-clock-budget-interval-this-week)
-                                     ("BUDGET_YEAR" org-clock-budget-interval-this-year))
-        org-clock-budget-ratio-faces '((1.0 hydra-face-red)
+  ;; set colors for different budget exhaustion states
+  (setq org-clock-budget-ratio-faces '((1.0 hydra-face-red)
                                        (0.95 font-lock-type-face)
                                        (0.5 ivy-confirm-face)
-                                       (0.0 font-lock-keyword-face))))
+                                       (0.0 font-lock-keyword-face))
+  ;; set time-format to h:mm
+        org-duration-format (quote h:mm))
+  ;; make popup-buffer larger
+  (set-popup-rule! "^\\*Org clock budget report" :size 0.35 :quit nil))
+
+;; some custom functions for displaying
+(defun show-yearly-clock-budget ()
+  "Show yearly org-clock budget"
+  (interactive)
+  (setq org-clock-budget-intervals '(("BUDGET_YEAR" org-clock-budget-interval-this-year)))
+  (org-clock-budget-report)
+  )
+
+(defun show-monthly-clock-budget ()
+  "Show monthly org-clock budget"
+  (interactive)
+  (setq org-clock-budget-intervals '(("BUDGET_MONTH" org-clock-budget-interval-this-month)))
+  (org-clock-budget-report)
+  )
+
+(defun show-weekly-clock-budget ()
+  "Show yearly org-clock budget"
+  (interactive)
+  (setq org-clock-budget-intervals '(("BUDGET_WEEK" org-clock-budget-interval-this-week)))
+  (org-clock-budget-report)
+  )
