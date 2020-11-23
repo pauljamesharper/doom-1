1f2b86eccce534108f684309f1d2b1a4789612a9
linozen <linus@sehn.tech>
2020-11-23 14:28:02 +0100

    agenda changes

--- config.el
+++ config.el
@@ -164,44 +164,43 @@
         org-roam-file-exclude-regexp ".*archive.org"))
 
 (after! org
-  (setq org-agenda-files (list org-directory)
-        org-habit-show-done-always-green 't)
-
-(use-package! org-super-agenda
-  :after org-agenda
-  :init
-  (setq org-agenda-window-setup 'current-window)
-  (setq org-agenda-start-day "+0d")
-  (setq org-agenda-span 'day)
-  (setq org-agenda-skip-scheduled-if-done t)
-  (setq org-agenda-skip-deadline-if-done t)
-  (setq org-agenda-start-on-weekday nil)
-  (setq org-agenda-dim-blocked-tasks nil) ;; makes main tasks visible in agenda-view
-  (setq org-agenda-files '("~/org/actions.org"
-                           "~/org/cs.org"
-                          "~/org/strategy.org"
-                          "~/org/reading.org"
-                          "~/org/watching.org"
-                          "~/org/calendar.org"
-                          "~/org/outreach.org"
-                          "~/org/caldav.org"))
-  (setq org-super-agenda-groups '((:name "Today"
-                                         :time-grid t)
-                                  (:name "Due today"
-                                          :deadline today)
-                                  (:name "Overdue"
-                                         :deadline past)
-                                  (:name "Due soon"
-                                          :deadline future)
-                                  (:name "Habits"
-                                         :habit t)
-                                  (:name "Reschedule or start"
-                                         :scheduled past)
-                                  (:name "Start today"
-                                          :scheduled today)
-                                  (:name "Start soon"
-                                          :scheduled future)
-                                  ))
+  (use-package! org-super-agenda
+    :after org-agenda
+    :init
+    (setq org-habit-show-done-always-green 't
+          org-agenda-prefix-format
+          '((agenda . " %?-12t% s")
+            (todo . " %i %-12:c")
+            (tags . " %i %-12:c")
+            (search . " %i %-12:c")))
+    (setq org-agenda-window-setup 'current-window)
+    (setq org-agenda-start-day "+0d")
+    (setq org-agenda-span 'day)
+    (setq org-agenda-skip-scheduled-if-done t)
+    (setq org-agenda-skip-deadline-if-done t)
+    (setq org-agenda-start-on-weekday nil)
+    (setq org-agenda-dim-blocked-tasks nil) ;; makes main tasks visible in agenda-view
+    (setq org-agenda-files '("~/Exocortex/org/actions.org"
+                             "~/Exocortex/org/actions-compsci.org"
+                             "~/Exocortex/org/calendar.org"
+                             "~/Exocortex/org/caldav.org"))
+    (setq org-super-agenda-groups '((:name "Today"
+                                     :time-grid t)
+                                    (:name "Due today"
+                                     :deadline today)
+                                    (:name "Overdue"
+                                     :deadline past)
+                                    (:name "Due soon"
+                                     :deadline future)
+                                    (:name "Habits"
+                                     :habit t)
+                                    (:name "Reschedule or start"
+                                     :scheduled past)
+                                    (:name "Start today"
+                                     :scheduled today)
+                                    (:name "Start soon"
+                                     :scheduled future)
+                                    ))
     :config
     (org-super-agenda-mode)))
 
